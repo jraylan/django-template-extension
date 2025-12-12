d2e1c0c275b1f2b3294482740f59f6e274f9d16b
Raylan <jeffersonraylan@gmail.com>
2025-12-12 05:41:56 -0300

    fix: remove unused file

--- manage.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-"""Dummy manage.py to simulate a Python Django project for testing."""
-if __name__ == '__main__':
-    print('This is a dummy manage.py for testing the extension.')
